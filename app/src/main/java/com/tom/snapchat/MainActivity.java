af8a094dc9a0e217fca7aa4c8f255679bd23eb21
Hank Tom <hanktom223@gmail.com>
2016-04-29 00:56:19 +0800

    產生並發送Notification

--- app/src/main/java/com/tom/snapchat/MainActivity.java
+++ app/src/main/java/com/tom/snapchat/MainActivity.java
@@ -1,5 +1,10 @@
 package com.tom.snapchat;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        makeNotification();
+    }
+
+    private void makeNotification() {
+        Bitmap bmp = BitmapFactory
+                .decodeResource(getResources(), R.drawable.pig64);
+        Notification.BigPictureStyle big =
+                new Notification.BigPictureStyle();
+        big.bigPicture(
+                BitmapFactory.decodeResource(getResources(), R.drawable.pig256))
+                .setSummaryText("bla bla bla");
+        Notification notification = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.pig32)
+                .setContentTitle("This is Title")
+                .setContentText("This is Text")
+                .setContentInfo("This is Info")
+                .setWhen(System.currentTimeMillis())
+                .build();
+        NotificationManager manager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        manager.notify(1, notification);
     }
 }
